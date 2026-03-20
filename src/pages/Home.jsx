4c9cb50c87da71feea3507b87f83f5300022584e
Matthew K <arcmatthewrk@gmail.com>
2026-03-20 20:29:41 +0200

    Include some files that werent staged to last commit.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,12 @@ import SectionWorks from "../sections/SectionWorks";
 import SectionBuild from "../sections/SectionBuild";
 import SectionAbout from "../sections/SectionAbout";
 import Footer from "../sections/SectionFooter";
+import Nav from "../sections/Nav";
 
 function HomePage() {
     return (
         <div className="home_page">
+            <Nav />
             <HeroSection />
             <SectionTagline />
             <SectionValueProposition />
