afe87273678af05da91524f202fb0086c4988dc9
Matthew K <arcmatthewrk@gmail.com>
2026-04-10 14:08:32 +0200

    Patch regressions: - nav_toggle stacking context behind images; - SectionWorks project wrappers hiding project text; - New event handeler function handleContactFromOverlay in overlay get in touch button (scroll lock still an issue).

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -21,6 +21,13 @@ function Nav({isOpen, toggleContactModal}) {
         navigate("/", {state: {scrollTo: sectionId}});
     }
 
+    function handleContactFromOverlay(event) {
+        event.stopPropagation();
+        toggleContactModal();
+        closeMenu();
+        console.log("ismenueopen", isMenuOpen, "isopen", isOpen);
+    }
+
     return(
         <NavRoot position='fixed' offsetTop='0rem' className='nav_root'>
             <NavBar direction='row' dock={`${isPastThreshold ? "right" : "none"}`} padding='none' maxWidth='md' className='nav_bar'>
@@ -46,7 +53,7 @@ function Nav({isOpen, toggleContactModal}) {
                     <Portal>
                         <NavOverlay onClick={closeMenu} className='nav_overlay'>
                             <GridContainer as='div' columns='repeat(12, 1fr)' rows='repeat(6, 1fr)' gap='lg'>
-                                <GridItem onClick={toggleMenu} as='div' col='10 / span 1' row='1 / span 1' className='nav_overlay_close_container'>
+                                <GridItem onClick={closeMenu} as='div' col='10 / span 1' row='1 / span 1' className='nav_overlay_close_container'>
                                     {isMenuOpen && (<Paragraph level='md' className='nav_close_toggle'>Close</Paragraph>)}
                                 </GridItem>
                                 <GridItem as='div' col='3 / span 3' row='3 / span 4' className='nav_menu_overlay_container' >
@@ -55,7 +62,7 @@ function Nav({isOpen, toggleContactModal}) {
                                         <Button as="button" onClick={() => goToSection("section_works")} className='nav_link_overlay'>Works</Button>
                                         <Button as="button" onClick={() => goToSection("section_build")} className='nav_link_overlay'>Capabilities</Button>
                                         <Button as="button" onClick={() => goToSection("section_about")} className='nav_link_overlay'>About</Button>
-                                        <Button as="button" className='button_secondary_container contact_overlay' onClick={toggleContactModal}>
+                                        <Button as="button" className='button_secondary_container contact_overlay' onClick={handleContactFromOverlay}>
                                             <Paragraph level='md' className='contact_overlay nav_contact_text'>Get in touch</Paragraph>
                                         </Button>
                                     </NavMenu>
