334d150d9ba7bdc8ae5dadaccc7ee054dfc9b365
Matthew K <arcmatthewrk@gmail.com>
2026-04-21 14:03:00 +0200

    Add about me placeholder text in json dataset. Rectify spacing and styling to case study project content. Adjust case study project title styling.

--- src/components/CaseStudyCoordinator.jsx
+++ src/components/CaseStudyCoordinator.jsx
@@ -6,23 +6,23 @@ function CaseStudyCoordinator({project}) {
 
         const COMPONENT_MAP = {
             pill: (item) => (
-                <Container as='div' direction='row' gap='md'>
+                <Container as='div' direction='row' gap='md' className='pill_container'>
                     {item.content.map((pill, index) => (
-                        <Pill as='p' key={index}>{pill}</Pill>
+                        <Pill as='p' key={index} className='case_study_pill'>{pill}</Pill>
                     ))}
                 </Container>
             ),
-            heading: (item) => <Heading level={item.level || 'h4'}>{item.content}</Heading>,
-            paragraph: (item) => <Paragraph level='md'>{item.content}</Paragraph>,
-            list: (item) => <List as={item.ordered ? "ol" : "ul"} items={item.content} />,
-            pullquote: (item) => <PullQuote>{item.content}</PullQuote>, 
-            quoteblock: (item) => <QuoteBlock cite={item.cite}>{item.content}</QuoteBlock>, 
-            inlinequote: (item) => <InlineQuote>{item.content}</InlineQuote>, 
-            callout: (item) => <Callout variant={item.variant}>{item.content}</Callout>,
+            heading: (item) => <Heading level={item.level || 'h4'} className='case_study_heading'>{item.content}</Heading>,
+            paragraph: (item) => <Paragraph level='md' className='case_study_paragraph'>{item.content}</Paragraph>,
+            list: (item) => <List as={item.ordered ? "ol" : "ul"} items={item.content} className='case_study_list'/>,
+            pullquote: (item) => <PullQuote className='case_study_pullquote bleed'>{item.content}</PullQuote>, 
+            quoteblock: (item) => <QuoteBlock cite={item.cite} className='case_study_quoteblock'>{item.content}</QuoteBlock>, 
+            inlinequote: (item) => <InlineQuote className='case_study_inlinequote'>{item.content}</InlineQuote>, 
+            callout: (item) => <Callout variant={item.variant} className='case_study_callout'>{item.content}</Callout>,
             collaborators: (item) => (
-                <Container as='div' direction='row' gap='none' className='case_study_collaborator wrapper'>
+                <Container as='div' direction='row' gap='4xl' className='case_study_collaborator_wrapper'>
                     {item.content.map((collaborator, index) => (
-                        <Collaborators key={index} collaborator={collaborator}/>
+                        <Collaborators key={index} collaborator={collaborator} />
                     ))}
                 </Container>
             ),
@@ -35,7 +35,7 @@ function CaseStudyCoordinator({project}) {
 
     return (
         <div>
-            <Container as='div' direction='column' gap='4xl' maxWidth='full' className='case_study_container'>
+            <Container as='div' direction='column' gap='none' maxWidth='full' className='case_study_container'>
                 <Container as='div' direction='row' gap='lg' maxWidth='full'>
                     <ImageWrapper className='case_study_image_wrapper'>
                         <Image src={project.media.images[0]["url"]} alt={project.media.images[0]["alt_text"]} className='case_study_hero_image'/>
--- src/sections/SectionCaseStudy.jsx
+++ src/sections/SectionCaseStudy.jsx
@@ -12,7 +12,7 @@ function SectionCaseStudy({project}) {
         <div>
             <Section as='section' maxWidth='full' className='section_case_study'>
                 <GridContainer as='div' columns='repeat(12,1fr)' rows='repeat(2,auto)' gap='lg'>
-                    <GridItem as='div' col='5 / span 6' row='1 / span 1'>
+                    <GridItem as='div' col='6 / span 6' row='1 / span 1'>
                         <Container as='div' direction='row' gap='md' maxWidth='full' className='case_study_heading_container'>
                             <Heading level='h2' >{`${me[3].heading[0]}`} <span class='heading_sm'>{`${me[3].heading[1]}`}</span></Heading>
                         </Container>
@@ -24,7 +24,7 @@ function SectionCaseStudy({project}) {
                             <Container as='div' direction='column' gap='xl' maxWidth='full' className='project_links_container' >
                                 {projectArray.map((project) => {
                                     return (<RouterLink to={`/project/${project.key}`}>
-                                        <Heading level='h6' id={project.project_title} className={project.key == id ? 'project_title--active' : 'project_title'}>
+                                        <Heading level='h6' id={project.project_title} className={project.key == id ? 'project_title--active' : 'project_title--inactive'}>
                                             {project.project_title}
                                         </Heading>
                                     </RouterLink>)
@@ -37,7 +37,7 @@ function SectionCaseStudy({project}) {
                     </GridItem>
 
 
-                    <GridItem as='div' col='5 / span 5' row='2 / span 1' >
+                    <GridItem as='div' col='6 / span 5' row='2 / span 1' >
                         <CaseStudyCoordinator project={project}></CaseStudyCoordinator>
                     </GridItem>
                 </GridContainer>
